e6fc6888fce031e8fbdba898090b0bfbba97a7ba
keisyaaninditakirani <keisyakeisya854@gmail.com>
2025-02-12 11:26:27 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,60 @@
+let namaDepan = "Keisya";
+let namaTengah = "Anindita";
+let namaBelakang = "Kirani";
+
+document.write("nama saya adalah " + namaDepan + " " + namaTengah + " " + namaBelakang);
+
+let alamatDepan = "Ngablak";
+let alamatTengah = "Sari";
+let alamatBelakang = "RT.04 RW.8";
+
+document.write(" " + "saya tinggal di " + alamatDepan + " " + alamatTengah + " " + alamatBelakang);
+
+let usia = "16";
+
+document.write(" " + "Pada tahun 2024 ini usia saya adalah " + usia + " tahun.");
+
+let ttlTempat = "Semarang";
+let ttlTanggal = "30-04-2008";
+
+document.write(" " + "Saya lahir di " + " " + ttlTempat + " " + ttlTanggal);
+
+let gender = "Perempuan";
+
+document.write(" " + "Jenis kelamin saya adalah " + " " + gender);
+
+let agama = "Islam";
+
+document.write(" " + "Saya beragama " + agama);
+
+let email = "andtakrni@gmail.com";
+
+document.write(" " + "Alamat email saya adalah" + " " + email);
+
+let berat = "49kg";
+
+document.write(" " + "Saya memiliki berat badan" + " " + berat);
+
+let tinggi = "157";
+
+document.write(" " + "dengan tinggi" + " " + tinggi + " cm. ");
+
+let hobi = "memasak";
+
+document.write( " " + "Saya sangat senang" + " " + hobi);
+
+let cita = " menjadi Dokter";
+
+document.write(" " + "karena saya memiliki cita cita" + " " + cita);
+
+let pekerjaan = "pelajar";
+
+document.write(" " + " Saat ini saya adalah seorang" + " " + pekerjaan);
+
+let sekola = "SMK NEGERI 1 SAYUNG";
+
+document.write(" " + "yang bersekolah di" + " " + sekola);
+
+let jurusan = "PPLG";
+
+document.write(" " + "pada jurusan" + " " + jurusan);
